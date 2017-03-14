da8085d1bd4d16cf58d05f56f3b479688a7ae46b
Unknown <dantedg@comcast.net>
2017-03-13 22:17:33 -0700

    tyty

--- data.sql
+++ data.sql
@@ -21,11 +21,11 @@ INSERT INTO Employee VALUES(8,4,'Sufjan Stevens', '292-283-1882', 'supervisor','
 INSERT INTO Employee VALUES(9,5,'Jeff Mangum', '292-283-1882', 'manager','17-Oct-1090');
 INSERT INTO Employee VALUES(10,5,'Guy Ferrari', '292-283-1882', 'supervisor','10-Dec-2010');
 
-INSERT INTO Property VALUES(1,1,2,3,1234.23,'leased','271st Street','San Deigo, CA','97362');
+INSERT INTO Property VALUES(1,1,2,3,1234.23,'available','271st Street','San Deigo, CA','97362');
 INSERT INTO Property VALUES(2,1,4,4,1400.33,'available','222nd Street','San Jose, CA','97222');
 INSERT INTO Property VALUES(3,2,6,5,1834.78,'available','17th Street','San Jose, CA','98283');
-INSERT INTO Property VALUES(4,2,8,2,1139.67,'leased','200th Street','San Fransico, CA','99162');
-INSERT INTO Property VALUES(5,3,10,3,1234.23,'leased','271st Street','San Deigo, CA','97362');
+INSERT INTO Property VALUES(4,2,8,2,1139.67,'available','200th Street','San Fransico, CA','99162');
+INSERT INTO Property VALUES(5,3,10,3,1234.23,'available','271st Street','San Deigo, CA','97362');
 INSERT INTO Property VALUES(6,4,8,8,1400.33,'available','222nd Street','San Jose, CA','97222');
 INSERT INTO Property VALUES(7,5,10,123,9990.78,'available','17th Street','San Jose, CA','98283');
 
