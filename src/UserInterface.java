963207e7b11a5c613ba5305c5c3780213b2f45c4
Alex Martin <gradient.am@gmail.com>
2017-03-13 22:45:04 -0700

    progress on UI

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,4 +1,26 @@
+import java.awt.*;
+import java.sql.*;
 import javax.swing.*;
 
 public class UserInterface extends JFrame {
+
+    private Connection connection;  // Oracle database connection object
+
+    public UserInterface() {
+        Container container = this.getContentPane();
+
+    }
+
+
+    public static void main(String[] args) {
+        // dynamically load the Oracle DB driver class
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Could not find DB driver class!");
+            return;
+        }
+
+        UserInterface ui = new UserInterface();
+    }
 }
