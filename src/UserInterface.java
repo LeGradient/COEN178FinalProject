5019cd90ed39feef2ec855753feb61c8b45b197c
Alex Martin <gradient.am@gmail.com>
2017-03-13 23:57:02 -0700

    Got DB driver working, added a bunch of buttons

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -8,7 +8,36 @@ public class UserInterface extends JFrame {
 
     public UserInterface() {
         Container container = this.getContentPane();
-        // useless comment
+        container.setLayout(new BorderLayout());
+        Font fontBtn = new Font(Font.SANS_SERIF, Font.PLAIN, 24);
+
+        // left-side button menu
+        JPanel menuPanel = new JPanel();
+        container.add(menuPanel, BorderLayout.LINE_START);
+        menuPanel.setLayout(new FlowLayout());
+        menuPanel.setPreferredSize(new Dimension(540, 0));
+        JButton[] procBtn = new JButton[10];
+        procBtn[0] = new JButton("Show Rentals By Branch");
+        procBtn[1] = new JButton("Show Supervisor Rentals");
+        procBtn[2] = new JButton("Show Rentals By Owner");
+        procBtn[3] = new JButton("Search Rentals");
+        procBtn[4] = new JButton("Count Rentals Per Branch");
+        procBtn[5] = new JButton("Create New Lease");
+        procBtn[6] = new JButton("Show Leases by Renter");
+        procBtn[7] = new JButton("Show Renters With Multiple Leases");
+        procBtn[8] = new JButton("Show Average Rent By Town");
+        procBtn[9] = new JButton("Show Leases That Expire Soon");
+        for (JButton button : procBtn) {
+            button.setPreferredSize(new Dimension(500, 50));
+            button.setFont(fontBtn);
+            menuPanel.add(button);
+        }
+
+
+        this.setSize(800, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
     }
 
 
@@ -18,7 +47,6 @@ public class UserInterface extends JFrame {
             Class.forName("oracle.jdbc.driver.OracleDriver");
         } catch (ClassNotFoundException e) {
             System.out.println("Could not find DB driver class!");
-            return;
         }
 
         UserInterface ui = new UserInterface();
