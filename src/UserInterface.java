1bb32bc7c05d71ac4f851926afbf738bebfb49d3
Unknown <dantedg@comcast.net>
2017-03-16 02:13:32 -0700

    new

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -139,7 +139,7 @@ public class UserInterface extends JFrame implements ActionListener {
                         "Property.rental_id, " +
                         "Property.street, " +
                         "Property.city, " +
-                        "Property.zip, " +
+                        "Property.zip " +
                         "FROM Property JOIN Employee " +
                         "ON Property.supervisor_id = Employee.emp_id";
                 Statement stmt = UserInterface.this.connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
@@ -180,9 +180,12 @@ public class UserInterface extends JFrame implements ActionListener {
 
     private class ProcPanel2 extends JPanel {
         private JScrollPane resultsPane = new JScrollPane();
+        
+        // extra information to be displayed above the table
+        private JLabel resultsOwnerLabel = new JLabel("Owner");
 
         public ProcPanel2() {
-            // initialize results area
+            this.setLayout(new BorderLayout());
 
             // initialize menu
             JPanel menuPanel = new JPanel();
@@ -191,7 +194,7 @@ public class UserInterface extends JFrame implements ActionListener {
             menuPanel.setBackground(Color.GRAY);
             this.add(menuPanel, BorderLayout.LINE_START);
 
-            JLabel branchLabel = new JLabel("Branch ID:");
+            JLabel branchLabel = new JLabel("Owner ID:");
             branchLabel.setFont(UserInterface.this.fontBtn);
 
             JTextField ownerField = new JTextField(5);
@@ -204,15 +207,26 @@ public class UserInterface extends JFrame implements ActionListener {
             menuPanel.add(ownerField);
             menuPanel.add(submitBtn);
 
+            // initialize results area
             JPanel resultsPanel = new JPanel(new BorderLayout());
+            JPanel labelPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 10));
+            labelPanel.add(resultsOwnerLabel);
+            resultsPanel.add(labelPanel, BorderLayout.PAGE_START);
+            this.add(resultsPanel, BorderLayout.CENTER);
 
             submitBtn.addActionListener(actionEvent -> {
+           
+
+                // get owners name
+               this.resultsOwnerLabel.setText("Owner ID: " + ownerField.getText());
+           
+
+                // get available properties
                 try {
                     // send query
-                    String sql =
-                            "SELECT rental_id, street, city, zip" +
-                            "FROM Property" +
-                            "WHERE owner_id = " + ownerField.getText();
+                    String sql = "SELECT rental_id, street, city, zip " +
+                    		"FROM Property " +
+                    		"WHERE owner_id = " + ownerField.getText();
                     Statement stmt = UserInterface.this.connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
                     ResultSet result = stmt.executeQuery(sql);
 
@@ -235,19 +249,182 @@ public class UserInterface extends JFrame implements ActionListener {
                         }
                     }
 
-                    this.remove(this.resultsPane);
+                    resultsPanel.remove(this.resultsPane);
                     this.resultsPane = new JScrollPane(new JTable(data, columns));
-                    this.add(this.resultsPane, BorderLayout.CENTER);
+                    resultsPanel.add(this.resultsPane, BorderLayout.CENTER);
                     this.revalidate();
                     this.repaint();
                 } catch (SQLException e) {
-                    System.out.println("Could not initialize procPanel[2]!");
+                    System.out.println("Could not initialize procPanel[0]!");
                     System.out.println(e);
                     System.exit(1);
                 }
             });
         }
     }
+
+    private class ProcPanel3 extends JPanel {
+        private JScrollPane resultsPane = new JScrollPane();
+        
+        // extra information to be displayed above the table
+        private JLabel resultsOwnerLabel = new JLabel("Owner");
+
+        public ProcPanel3() {
+            this.setLayout(new BorderLayout());
+
+            // initialize menu
+            JPanel menuPanel = new JPanel();
+            menuPanel.setLayout(new FlowLayout());
+            menuPanel.setPreferredSize(new Dimension(300, 0));
+            menuPanel.setBackground(Color.GRAY);
+            this.add(menuPanel, BorderLayout.LINE_START);
+
+            JLabel rentLabelMin = new JLabel("Min Rent: ");
+            rentLabelMin.setFont(UserInterface.this.fontBtn);
+
+            JTextField rentMinField = new JTextField(12);
+            rentMinField.setFont(UserInterface.this.fontBtn);
+
+            JLabel rentLabelMax = new JLabel("Max Rent: ");
+            rentLabelMax.setFont(UserInterface.this.fontBtn);
+            
+            JTextField rentMaxField = new JTextField(12);
+            rentMaxField.setFont(UserInterface.this.fontBtn);
+            
+            JLabel cityLabel  = new JLabel("City : ");
+            cityLabel.setFont(UserInterface.this.fontBtn);
+
+            JTextField cityField = new JTextField(15);
+            cityField.setFont(UserInterface.this.fontBtn);
+
+            JLabel roomAmountLabel = new JLabel("Room amount: ");
+            roomAmountLabel.setFont(UserInterface.this.fontBtn);
+            
+            JTextField roomAmountField = new JTextField(5);
+            roomAmountField.setFont(UserInterface.this.fontBtn);
+       
+            JButton submitBtn = new JButton("Submit");
+            submitBtn.setFont(UserInterface.this.fontBtn);
+
+            menuPanel.add(rentLabelMax);
+            menuPanel.add(rentMaxField);
+            menuPanel.add(rentLabelMin);
+            menuPanel.add(rentMinField);
+            menuPanel.add(cityLabel);
+            menuPanel.add(cityField);
+            menuPanel.add(roomAmountLabel);
+            menuPanel.add(roomAmountField);
+            menuPanel.add(submitBtn);
+
+            // initialize results area
+            JPanel resultsPanel = new JPanel(new BorderLayout());
+            JPanel labelPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 10));
+            labelPanel.add(resultsOwnerLabel);
+            resultsPanel.add(labelPanel, BorderLayout.PAGE_START);
+            this.add(resultsPanel, BorderLayout.CENTER);
+
+            submitBtn.addActionListener(actionEvent -> {
+           
+                // get available properties
+                try {
+                    // send query
+                    String sql = 
+						"SELECT rental_id, street, city, zip " +
+						"FROM Property " +
+						"WHERE status = 'available' " +
+						"AND num_rooms = " + roomAmountField.getText() +
+						"AND city = " + "'" + cityField.getText() + "'" +
+ 						"AND monthly_rent >= " + rentMinField.getText() +
+						"AND monthly_rent <= " + rentMaxField.getText();
+                    Statement stmt = UserInterface.this.connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                    ResultSet result = stmt.executeQuery(sql);
+
+                    // get column names
+                    int colCount = result.getMetaData().getColumnCount();
+                    String[] columns = new String[colCount];
+                    for (int i = 0; i < result.getMetaData().getColumnCount(); i++){
+                        columns[i] = result.getMetaData().getColumnName(i + 1);
+                    }
+
+                    // get data
+                    result.last();
+                    int rowCount = result.getRow();
+                    result.beforeFirst();
+                    String[][] data = new String[rowCount][colCount];
+                    for (int i = 0; i < rowCount; i++) {
+                        result.next();
+                        for (int j = 0; j < colCount; j++) {
+                            data[i][j] = result.getString(j + 1);
+                        }
+                    }
+
+                    resultsPanel.remove(this.resultsPane);
+                    this.resultsPane = new JScrollPane(new JTable(data, columns));
+                    resultsPanel.add(this.resultsPane, BorderLayout.CENTER);
+                    this.revalidate();
+                    this.repaint();
+                } catch (SQLException e) {
+                    System.out.println("Could not initialize procPanel[3]!");
+                    System.out.println(e);
+                    System.exit(1);
+                }
+            });
+        }
+    }
+
+    private class ProcPanel4 extends JPanel {
+        private JScrollPane resultsPane = new JScrollPane();
+
+        public ProcPanel4() {
+            // initialize results area
+
+            JPanel resultsPanel = new JPanel(new BorderLayout());
+
+            try {
+                // send query
+                String sql = "SELECT branch_id, COUNT(*) num " +
+                			"FROM Property JOIN Employee " +
+                			"ON Property.supervisor_id = Employee.emp_id " +
+                			"WHERE Property.status = 'available' " +
+                			"GROUP BY branch_id";
+                Statement stmt = UserInterface.this.connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                ResultSet result = stmt.executeQuery(sql);
+
+                // get column names
+                int colCount = result.getMetaData().getColumnCount();
+                String[] columns = new String[colCount];
+                for (int i = 0; i < result.getMetaData().getColumnCount(); i++){
+                    columns[i] = result.getMetaData().getColumnName(i + 1);
+                }
+
+                // get data
+                result.last();
+                int rowCount = result.getRow();
+                result.beforeFirst();
+                String[][] data = new String[rowCount][colCount];
+                for (int i = 0; i < rowCount; i++) {
+                    result.next();
+                    for (int j = 0; j < colCount; j++) {
+                        data[i][j] = result.getString(j + 1);
+                    }
+                }
+
+                this.remove(this.resultsPane);
+                this.resultsPane = new JScrollPane(new JTable(data, columns));
+                this.add(this.resultsPane, BorderLayout.CENTER);
+                this.revalidate();
+                this.repaint();
+            } catch (SQLException e) {
+                System.out.println("Could not initialize procPanel[4]!");
+
+                System.out.println(e);
+                System.exit(1);
+            }
+        }
+    }
+
+
+           
 
 
 
@@ -292,7 +469,10 @@ public class UserInterface extends JFrame implements ActionListener {
         // init panels
         this.procPanel[0] = new ProcPanel0();
         this.procPanel[1] = new ProcPanel1();
-
+        this.procPanel[2] = new ProcPanel2();
+        this.procPanel[3] = new ProcPanel3();
+        this.procPanel[4] = new ProcPanel4();
+        
         this.setSize(1200, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
